37830ef3a1a82183f6a0c547067941b89f15ee32
Abdullah-Official <rugusakii@gmail.com>
2023-01-10 05:11:44 +0500

    Problem Solving - BitManipulation

--- BitWise_Operators/NegativeDecToBinary.cpp
+++ BitWise_Operators/NegativeDecToBinary.cpp
@@ -1,32 +1,34 @@
-// #include<iostream>
-// #include<math.h>
-// using namespace std;
+#include<iostream>
+#include<math.h>
+using namespace std;
 
 
-// int main(){
+int main(){
 
-//      int n;
-//     cin >> n;
-//     int absoluteVal = abs(n);
-//     // cout << absoluteVal;
+     int n;
+    cin >> n;
+    int absoluteVal = abs(n);
+    // cout << absoluteVal;
 
-//     int ans  = 0;
-//     int i = 0;
-//     while(absoluteVal != 0 ) {
+    int ans  = 0;
+    int i = 0;
+    while(absoluteVal != 0 ) {
 
-//         int bit = absoluteVal&1;
-//         ans = (bit*pow(10,i)) + ans;
-//         absoluteVal=absoluteVal>>1;
-//         i++;
+        int bit = absoluteVal&1;
+        ans = (bit*pow(10,i)) + ans;
+        absoluteVal=absoluteVal>>1;
+        i++;
 
-//     }
-
-//     int onesComp;
-//     int k = 0;
-//     for(int j = 0; j<31; j++){
-//         if(k <=)
-//     }
+    }
+    int comp;
+    int j = 0;
+   while(ans!=0){
+        int convertedBit = ans^1;
+        cout<< convertedBit;
+        ans = ans >> 1;
+        j++;
+   }
     
     
-//     return 0;
-// }
+    return 0;
+}
